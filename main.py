6e5bb8be000c4786f3fa6fb3fd25470beb3a4740
Jabanes <erezhabani2003@gmail.com>
2025-01-29 10:29:30 +0200

    CRITICAL WARNING

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"CRITICAL! WARNING! CRITICAL WARNING! CRITICAL! WARNING! CRITICAL WARNING! CRITICAL! WARNING! CRITICAL WARNING!"}
